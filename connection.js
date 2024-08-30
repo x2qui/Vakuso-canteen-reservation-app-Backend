1c9359b4e4ccacb66fa77d588fbb3497026a242e
x2qui <130788820+x2qui@users.noreply.github.com>
2024-08-30 11:53:48 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/vakuso")
+mongoose.connect("mongodb://***/vakuso")
 .then(()=> console.log('Server is running'))
 .catch(err => console.log(err))
